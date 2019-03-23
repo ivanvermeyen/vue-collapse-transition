ee422a3c36ba0b8e1e3b2cd2c3a948255a8d13d0
Ivan Vermeyen <ivan@vermeyen.org>
2019-03-23 11:41:13 +0100

    Enable script tag usage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import CollapseTransition from './CollapseTransition'
 
 export default CollapseTransition
+
+export {
+  CollapseTransition,
+}
+
+export function install(Vue) {
+  Vue.component('CollapseTransition', CollapseTransition)
+}
+
+const plugin = {
+  install,
+}
+
+let GlobalVue = null
+
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
